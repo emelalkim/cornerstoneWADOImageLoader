b8acc3b4efd29be365632ba6f76aeec388c284f2
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2023-05-11 18:46:38 -0700

    Fix errors during build

--- src/imageLoader/wadouri/getPixelData.js
+++ src/imageLoader/wadouri/getPixelData.js
@@ -2,19 +2,19 @@ import getEncapsulatedImageFrame from './getEncapsulatedImageFrame.js';
 import getUncompressedImageFrame from './getUncompressedImageFrame.js';
 
 function getPixelData(dataSet, frameIndex = 0) {
-  console.log(" ----> getPixelData")
+  console.log(' ----> getPixelData');
   const pixelDataElement =
     dataSet.elements.x7fe00010 || dataSet.elements.x7fe00008;
 
   if (!pixelDataElement) {
-    console.log(" --- No pixel data returns null ---")
+    console.log(' --- No pixel data returns null ---');
     return null;
   }
 
-  console.log(" ----> !!pixelDataElement.encapsulatedPixelData", !!pixelDataElement.encapsulatedPixelData);
+  console.log(' ----> !!pixelDataElement.encapsulatedPixelData', Boolean(pixelDataElement.encapsulatedPixelData));
 
   if (pixelDataElement.encapsulatedPixelData) {
-    console.log(" ===> passed in if !!pixelDataElement.encapsulatedPixelData")
+    console.log(' ===> passed in if !!pixelDataElement.encapsulatedPixelData');
     return getEncapsulatedImageFrame(dataSet, frameIndex);
   }
 
